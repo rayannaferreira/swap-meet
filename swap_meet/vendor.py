f532ab1b19e57207c2c5991f1c85ef26a92515c1
Rayanna Ferreira <rayanna_ferreira@icloud.com>
2026-09-21 14:43:53 -0700

    complete wave 1 and tests wave 01

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,22 @@
 class Vendor:
-    pass
+    def __init__(self,inventory=None):
+        if inventory is None:
+            self.inventory=[]
+        else:    
+            self.inventory = inventory
+    def add(self, item):
+        self.inventory.append(item)
+        return item 
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+
+        return False
+
+    
+    #     def add_to_list_ok(word, word_list=None):
+    # word_list = [] if word_list is None else word_list
+    # word_list.append(word)  # if word_list not supplied, defaults to None, resulting in a new
+    # return word_list        # list being created with each invocation
