0fae15cef70ab64fe64caccc70b8b19448d6a391
rayne <pothong.r@gmail.com>
2026-09-22 14:17:32 -0700

    refactor decor class to include condition and pass tests

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,7 +1,7 @@
-from .item import Item
+from swap_meet.item import Item
 class Decor(Item):
-    def __init__(self, id=None, width=0, length=0):
-        super().__init__(id)
+    def __init__(self, id=None, width=0, length=0, condition=0):
+        super().__init__(id, condition)
         self.width = width
         self.length = length
 
