688d3305f6765a0158aac6db45d955535d33b13d
rayne <pothong.r@gmail.com>
2026-09-22 13:46:08 -0700

    implement clothing class

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,9 @@
-class Clothing:
-    pass
+from .item import Item
+
+class Clothing(Item):
+    def __init__(self, id=None, fabric="Unknown"):
+        super().__init__(id)
+        self.fabric = fabric
+
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
