fbd6c8b20c42e44e0042741264ae3fd84e42b575
rayne <pothong.r@gmail.com>
2026-09-22 14:11:05 -0700

    refactor item class to include condition attribute

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,10 +1,11 @@
 #wave 2
 import uuid
 class Item:
-    def __init__(self,id=None): 
+    def __init__(self,id=None, condition=0): 
         if id is  None :
             id = uuid.uuid4().int   
         self.id = id
+        self.condition = condition
 
 
     def get_category (self):
@@ -13,3 +14,20 @@ class Item:
 
     def __str__(self):
         return f"An object of type Item with id {self.id}."
+
+# wave 5
+    def condition_description(self):
+        if self.condition == 5:
+            return "mint"
+        elif self.condition == 4:
+            return "pristine"
+        elif self.condition == 3:
+            return "good"
+        elif self.condition == 2:
+            return "acceptable"
+        elif self.condition == 1:
+            return "heavily used"
+        else:
+            return "just throw this garbage away"
+    
+
