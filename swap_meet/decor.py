fcfdf05051f6b6e7849e2d2677e1adfb740a7a39
rayne <pothong.r@gmail.com>
2026-09-22 13:58:50 -0700

    implement decor class

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,11 @@
-class Decor:
-    pass
+from .item import Item
+class Decor(Item):
+    def __init__(self, id=None, width=0, length=0):
+        super().__init__(id)
+        self.width = width
+        self.length = length
+
+    def __str__(self):
+        return f"An object of type Decor with id {self.id}. It takes up a {self.width} by {self.length} sized space."
+
+
