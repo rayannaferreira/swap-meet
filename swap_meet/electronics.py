1bf38d2eb605383b1903152be0a3d63e8fd7b4af
rayne <pothong.r@gmail.com>
2026-09-22 13:59:32 -0700

    implement electronics class

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,9 @@
-class Electronics:
-    pass
+from .item import Item
+
+class Electronics(Item):
+    def __init__(self, id=None,type="Unknown"):
+        super().__init__(id)
+        self.type = type
+
+    def __str__(self):
+        return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
