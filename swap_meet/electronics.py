d0e744689ba15809b2fd842d4decccf929319034
rayne <pothong.r@gmail.com>
2026-09-22 14:19:15 -0700

    refactor electronics class to include condition and pass test

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,8 +1,8 @@
-from .item import Item
+from swap_meet.item import Item
 
 class Electronics(Item):
-    def __init__(self, id=None,type="Unknown"):
-        super().__init__(id)
+    def __init__(self, id=None,type="Unknown", condition=0):
+        super().__init__(id, condition)
         self.type = type
 
     def __str__(self):
