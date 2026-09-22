908ce95d84b8ab42715c15b5c2a07f94ed591f65
Rayanna Ferreira <rayanna_ferreira@icloud.com>
2026-09-22 13:18:19 -0700

    complete wave 3 and tests wave 3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -9,3 +9,7 @@ class Item:
 
     def get_category (self):
         return self.__class__.__name__
+ #wave 3
+
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -27,3 +27,14 @@ class Vendor:
         return None   
 
     
+#wave 3  
+
+    def swap_items(self,other_vendor,my_item,their_item):
+        if my_item  in self.inventory and their_item in other_vendor.inventory:
+            self.inventory.remove(my_item)
+            other_vendor.inventory.append(my_item)
+
+            other_vendor.inventory.remove(their_item)
+            self.inventory.append(their_item)
+            return True 
+        return False
