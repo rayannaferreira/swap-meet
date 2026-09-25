97f0c2744a36f2d46f780549c7144f375d99f2f3
rayne <pothong.r@gmail.com>
2026-09-25 10:06:50 -0700

    add get_category method to clothing class

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -6,5 +6,9 @@ class Clothing(Item):
         self.fabric = fabric
 
 
+    def get_category(self):
+        return "Clothing"
+
+
     def __str__(self):
         return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
