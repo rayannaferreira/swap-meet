643c5cfbdbc0e1205b3e2e7709817e212b78a389
rayne <pothong.r@gmail.com>
2026-09-25 10:05:47 -0700

    add get_category method to decor class

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -6,6 +6,10 @@ class Decor(Item):
         self.length = length
 
 
+    def get_category(self):
+        return "Decor"
+
+
     def __str__(self):
         return f"An object of type Decor with id {self.id}. It takes up a {self.width} by {self.length} sized space."
 
